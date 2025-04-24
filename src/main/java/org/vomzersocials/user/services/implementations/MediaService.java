fa533a88584a47fccc1c2d474d6f1d6481fe5278
JohDan <chinedunomeh2015@gmail.com>
2025-04-24 05:47:32 +0100

    S3Presigner configured in the media service

--- src/main/java/org/vomzersocials/user/services/implementations/MediaService.java
+++ src/main/java/org/vomzersocials/user/services/implementations/MediaService.java
@@ -10,15 +10,15 @@ import org.vomzersocials.user.utils.Media;
 import org.vomzersocials.user.utils.MediaType;
 import org.vomzersocials.user.data.repositories.MediaRepository;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
 import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.time.Duration;
 import java.util.List;
 import java.util.UUID;
@@ -33,7 +33,7 @@ public class MediaService {
     private S3Client s3Client;
 
     @Autowired
-    private S3Presigner s3Presigner; // Add this for presigned URL generation
+    private S3Presigner s3Presigner;
 
     @Value("${vomzer.bucket-name}")
     private String bucketName;
@@ -41,18 +41,14 @@ public class MediaService {
     @Value("${vomzer.cdn-url}")
     private String cdnUrl;
 
-    // Fetch all media with pagination
     public List<Media> getAll(int page, int size) {
-        // Using Spring Data pagination
         Page<Media> mediaPage = mediaRepository.findAll(PageRequest.of(page, size));
         return mediaPage.getContent();
     }
 
-    // Upload media to S3 (Walrus)
     public Media uploadMedia(MultipartFile file) throws IOException {
         String contentType = file.getContentType();
 
-        // Decide the folder based on media type
         String folder;
         MediaType mediaType;
 
@@ -69,7 +65,6 @@ public class MediaService {
         String originalFilename = file.getOriginalFilename();
         String uniqueKey = folder + UUID.randomUUID() + "-" + originalFilename;
 
-        // Upload to Walrus (S3)
         PutObjectRequest putRequest = PutObjectRequest.builder()
                 .bucket(bucketName)
                 .key(uniqueKey)
@@ -82,7 +77,6 @@ public class MediaService {
             throw new IOException("Failed to upload to S3: " + e.getMessage(), e);
         }
 
-        // Save media metadata
         Media media = new Media();
         media.setFilename(originalFilename);
         media.setMediaType(mediaType);
@@ -91,27 +85,23 @@ public class MediaService {
         return mediaRepository.save(media);
     }
 
-    public String generatePresignedUploadUrl(String originalFilename, String folder) {
-        // Generate a unique key for the uploaded file
+    public String generatePresignedUploadUrl(String originalFilename, String folder, String contentType) {
         String key = folder + "/" + UUID.randomUUID() + "-" + originalFilename;
 
-        // Create the PutObjectRequest to specify the details of the upload
-        PutObjectRequest objectRequest = PutObjectRequest.builder()
-                .bucket(bucketName) // Your bucket name
-                .key(key) // The unique key for the uploaded file
-                .contentType("image/jpeg") // You can dynamically set this based on file type
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .contentType(contentType)
                 .build();
 
-        // Generate a presigned URL with an expiration of 10 minutes
-        PresignedPutObjectRequest presignedRequest = s3Presigner.presignPutObject(
-                PresignedPutObjectRequest.builder()
-                        .putObjectRequest(objectRequest) // The PutObjectRequest
-                        .signatureDuration(Duration.ofMinutes(10)) // URL expiration time
-                        .build()
-        );
+        PutObjectPresignRequest preSignRequest = PutObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(10))
+                .putObjectRequest(putObjectRequest)
+                .build();
 
-        // Return the URL for the client to use
-        return presignedRequest.url().toString();
+        PresignedPutObjectRequest preSignedRequest = s3Presigner.presignPutObject(preSignRequest);
+
+        return preSignedRequest.url().toString();
     }
 
 }
